c25cea11a40b8e7f8f983e00e1b0dc22bcd31ee7
Jason Holland <jholland918@gmail.com>
2018-10-09 16:50:16 +0000

    init

---
+++ AcmeLogger/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcmeLogger
+{
+    public class Class1
+    {
+        public void Log(string message)
+        {
+            Debug.WriteLine(message);
+        }
+    }
+}
